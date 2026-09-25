cd468c28c6de927fe4ae7cc0c4d3f664eb57a017
Kindmanners <maxiefeseymen@gmail.com>
2026-09-25 12:00:21 +0200

    doc.go

---
+++ internal/executil/doc.go
@@ -0,0 +1,12 @@
+// Copyright (C) 2026 kindmanners on github
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Package executil contains platform-specific process launch, lifetime, and
+// termination helpers shared by Tether's managed subprocesses.
+package executil
+
+//Does nothing but don't remove :P 
